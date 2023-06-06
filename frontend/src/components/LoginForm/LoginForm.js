c6fd7ee931571896c86f9be44f540bb09b5d6997
aycelacap <[raycel5lacap@gmail.com]>
2023-06-06 14:11:34 -0400

    Remove Debuggers

--- frontend/src/components/LoginForm/LoginForm.js
+++ frontend/src/components/LoginForm/LoginForm.js
@@ -14,7 +14,7 @@ function LoginForm() {
     const history = useHistory();
 
     const demoLogin = (e) => {
-        debugger
+        // debugger
         e.preventDefault();
         e.stopPropagation();
         dispatch(sessionActions.login({
@@ -26,7 +26,7 @@ function LoginForm() {
     }
 
     const handleSubmit = (e) => {
-        debugger
+        // debugger
         e.preventDefault();
         e.stopPropagation();
         console.log(e, 'from handleSubmit')
